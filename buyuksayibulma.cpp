4dc1d7527bcf2d8451c52ae35979fe219ee3bbef
Linbats <115425138+Linbats@users.noreply.github.com>
2022-10-28 22:20:47 +0300

    Add files via upload

---
+++ buyuksayibulma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int sin1,sin2;
+cout << "Bu program iki sayı arasında büyük olanı bulur"<< "\nİlk sayıyı giriniz: "; cin >> sin1;
+cout << "İkinci sayıyı giriniz: "; cin >> sin2;
+if(sin1>sin2){
+cout << "İlk girdiğiniz sayı daha büyüktür ";
+
+}
+else{
+cout << "İkinci girdiğiniz sayı daha büyüktür ";
+}
+
+
+
+
+    return 0;
+}
