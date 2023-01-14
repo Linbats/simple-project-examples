8a61511ffc5abf58ab4bdfa5cc0a2d180c9f5978
Linbats <115425138+Linbats@users.noreply.github.com>
2023-01-14 17:18:31 +0300

    Add files via upload

---
+++ basicai.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string input;
+    cout << "selam nasılsın , hayat nasıl gidiyor:";
+    cin >> input;
+        if(input=="iyiyim","iyi","güzel" ,"idare"){
+            cout << "hep daha iyi ol  (: :";
+
+        }
+
+
+            else{
+                cout << "Niye yaa ? derdini anlatta beraber çözelim :";
+
+            }
+
+
+    return 0;
+}
