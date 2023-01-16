bd639d060319f0d953969ab49bcc055fe36b6ad1
Linbats <115425138+Linbats@users.noreply.github.com>
2023-01-16 19:36:05 +0300

    Add files via upload

---
+++ yes++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string input;
+    cout << "Lütfen döngüye giricek değeri giriniz\n>"; cin >> input;
+        for(int i=0; i<=5;){
+            cout << input << endl;
+        }
+    
+
+
+
+
+    return 0;
+}
